15566012aed05d81ec9ab5104ad6066bef4e425d
Tuer Hulz <darkost12@gmail.com>
2020-01-12 17:24:05 +0300

    v0.9.2 - quickfix

--- config.py
+++ config.py
@@ -33,8 +33,8 @@ CONFIRMATIONS = int(getenv_or_exit("CONFIRMATIONS"))
 TARGET = int(getenv_or_exit("TARGET"))
 TARGET_TIME = int(getenv_or_exit("TARGET_TIME"))
 ADDRESS = getenv_or_exit("ADDRESS")
-if web3.isAddress(ADDRESS) is False:
-    if web3.isChecksumAddress(ADDRESS) is False:
+if not web3.isAddress(ADDRESS):
+    if not web3.isChecksumAddress(ADDRESS):
         sys.exit("Invalid ADDRESS granted")
 else:
     ADDRESS = web3.toChecksumAddress(ADDRESS)
@@ -45,25 +45,25 @@ ACCOUNT = Account.privateKeyToAccount(PRIV_KEY)
 # Configuration warnings.
 if TARGET * ((CONFIRMATIONS + 1) * 16.5) > TARGET_TIME:
     print(
-        'Strongly advising you to reconsider the configuration!'
-        '\nAccording to average mining and confirmation speed,'
-        'this is nearly impossible. Performance is not guaranteed.'
-        '\nAlso it can lead to excessive expenditures.'
+        "Strongly advising you to reconsider the configuration!"
+        "\nAccording to average mining and confirmation speed,"
+        "this is nearly impossible. Performance is not guaranteed."
+        "\nAlso it can lead to excessive expenditures."
     )
 elif TARGET_TIME / (TARGET * 60) <= 1:
     print(
-        'Current configuration targets are hard to reach'
-        'due to possible network fluctuations.'
+        "Current configuration targets are hard to reach"
+        "due to possible network fluctuations."
     )
 
-
-BASE_PRICE = int(web3.eth.gasPrice / 10)
+# May vary from the current situation on Blockchain.
+BASE_PRICE = int(web3.eth.gasPrice / 1)
 
 
 # Creates contract instance.
-if os.path.exists("abi.json") is True and os.path.isfile("abi.json") is True:
+if os.path.exists("abi.json") and os.path.isfile("abi.json"):
     with open("abi.json") as file:
         abi = json.load(file)
     INSTANCE = web3.eth.contract(address=ADDRESS, abi=abi)
 else:
-    sys.exit("ABI should be present in file \'abi.json\'")
+    sys.exit("ABI should be present in file 'abi.json'")
